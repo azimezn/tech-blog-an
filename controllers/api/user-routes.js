bc2ec6c6b94bc3339758d5bfc53c272e2ea5512b
azimezn <117550865+azimezn@users.noreply.github.com>
2023-01-12 11:01:13 -0500

    added create user and login/logout in user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,10 +4,72 @@ const User = require('../../models/User');
 // CREATE a new user
 router.post('/', async (req, res) => {
     try {
-        const userData = await User.create(req.body);
-        res.status(200).json(userData);
+        const userData = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            res.status(200).json(userData);
+        });
+
     } catch (err) {
-        res.status(400).json(err);
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        });
+
+        if (!userData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect username or password. Please try again!' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect username or password. Please try again!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            console.log(
+                '🚀 ~ file: user-routes.js ~ line 57 ~ req.session.save ~ req.session.cookie',
+                req.session.cookie
+            );
+
+            res
+                .status(200)
+                .json({ user: userData, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
     }
 });
 
