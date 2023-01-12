08c29aaa31a15a2daa2e5b624e55cb1af6fa486f
azimezn <117550865+azimezn@users.noreply.github.com>
2023-01-12 11:57:15 -0500

    added utils and auth.js, but didnt integrate it in the code yet

--- models/index.js
+++ models/index.js
@@ -11,15 +11,6 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-Post.hasMany(Comment, {
-    foreignKey: 'post_id',
-    onDelete: 'CASCADE',
-});
-
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id',
-});
-
 User.hasMany(Comment, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE',
@@ -27,6 +18,15 @@ User.hasMany(Comment, {
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
 });
 
 module.exports = { User, Post, Comment };
